9f7c380fa6e7fefdf4ac307169af72f35cacc2d3
Ilya Rudnev <ilyuharud@gmail.com>
2024-02-23 00:04:25 +0300

    Remove unused imports

--- src/main/java/ru/otus/highloadarchitect/homework/socialnetwork/config/ApplicationConfig.java
+++ src/main/java/ru/otus/highloadarchitect/homework/socialnetwork/config/ApplicationConfig.java
@@ -3,7 +3,6 @@ package ru.otus.highloadarchitect.homework.socialnetwork.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,9 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.otus.highloadarchitect.homework.socialnetwork.dao.UserDao;
-
-import javax.sql.DataSource;
-import java.util.UUID;
 
 @Configuration
 @RequiredArgsConstructor
